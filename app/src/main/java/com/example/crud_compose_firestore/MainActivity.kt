4f492d908af76ae0c7a250619513e290c9893c4d
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-23 15:07:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/crud_compose_firestore/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.crud_compose_firestore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.crud_compose_firestore.ui.theme.CRUDcomposefirestoreTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CRUDcomposefirestoreTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CRUDcomposefirestoreTheme {
+        Greeting("Android")
+    }
+}
