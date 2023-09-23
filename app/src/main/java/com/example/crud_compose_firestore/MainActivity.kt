b871d04bb25b9f46725bc41bfe905cc17ad406e5
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-24 00:38:15 +0530

    making a scaffold

--- app/src/main/java/com/example/crud_compose_firestore/MainActivity.kt
+++ app/src/main/java/com/example/crud_compose_firestore/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.crud_compose_firestore
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.crud_compose_firestore.ui.theme.CRUDcomposefirestoreTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +31,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Scaffold(
+                        topBar = {
+                            Text(text = "CRUD Compose Firestore")
+                        }
+                    ) {
 
+                    }
                 }
             }
         }
