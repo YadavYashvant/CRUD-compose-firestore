cdba792c101654a09701a83013d8011d61c39af0
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-24 23:43:13 +0530

    getting data from firestore

--- app/src/main/java/com/example/crud_compose_firestore/MainActivity.kt
+++ app/src/main/java/com/example/crud_compose_firestore/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -164,7 +165,9 @@ fun firebaseUI(context: android.content.Context) {
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Reading from Firebase",
             fontSize = 18.sp,
-            fontWeight = FontWeight.Bold)
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally),
+        )
     }
 
 
