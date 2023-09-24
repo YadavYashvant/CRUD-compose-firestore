7ef58fe552de752d2b8e732ed23c8cbf5cbbfa2f
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-24 18:20:58 +0530

    made theme changes

--- app/src/main/java/com/example/crud_compose_firestore/MainActivity.kt
+++ app/src/main/java/com/example/crud_compose_firestore/MainActivity.kt
@@ -20,14 +20,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.crud_compose_firestore.presentation.models.User
 import com.example.crud_compose_firestore.ui.theme.CRUDcomposefirestoreTheme
 import com.google.firebase.firestore.CollectionReference
@@ -47,7 +51,12 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Scaffold(
                         topBar = {
-                            Text(text = "CRUD Compose Firestore")
+                            Text(text = "CRUD Compose Firestore", modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                                textAlign = TextAlign.Center,
+                                fontSize = 25.sp,
+                                fontWeight = FontWeight.Bold
+
+                            )
                         }
                     ) {
                         firebaseUI(LocalContext.current)
@@ -77,15 +86,20 @@ fun firebaseUI(context: android.content.Context) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(vertical = 48.dp, horizontal = 16.dp)
             .fillMaxWidth()
             .background(Color.White)
     ) {
         TextField(
             value = name.value,
+            shape = MaterialTheme.shapes.large,
             onValueChange = { name.value = it },
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text("Enter your name: ") }
+            modifier = Modifier.fillMaxWidth().padding(top = 32.dp).height(70.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            ),
+            label = { Text("Enter your name ") }
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -93,8 +107,13 @@ fun firebaseUI(context: android.content.Context) {
         TextField(
             value = branch.value,
             onValueChange = { branch.value = it },
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text("Enter your branch: ") }
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.fillMaxWidth().height(70.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            ),
+            label = { Text("Enter your branch ") }
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -102,22 +121,28 @@ fun firebaseUI(context: android.content.Context) {
         TextField(
             value = skill.value,
             onValueChange = { skill.value = it },
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text("Enter your skill: ") }
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.fillMaxWidth().height(70.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            ),
+            label = { Text("Enter your skill ") }
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedButton(
+        Button(
             onClick = {
                 val user = User(name.value, branch.value, skill.value)
                 addToFirebase(name.value,
                     branch.value,
                     skill.value,
                     context)
-            }
+            },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 32.dp, horizontal = 32.dp).height(70.dp)
         ) {
-            Text(text = "Add User")
+            Text(text = "Add User", fontSize = 18.sp)
         }
     }
 
